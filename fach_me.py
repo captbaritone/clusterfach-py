523df2cdfa5a02ac0ddc5f3868fa1935db80a79b
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-16 23:03:26 -0700

    Add fachme stuff

--- fach_me.py
+++ fach_me.py
@@ -1,6 +1,24 @@
+import sys
+import re
 from clusterfach import fach_me
 
-suggested_character_ids = fach_me(['502'])
+character_names = {}
 
-for character in suggested_character_ids[:10]:
-    print "Character: %s (%s)" % (character['id'], character['score'])
+p = re.compile("(\d+): (.*)")
+for line in open("characters.txt", "rb"):
+    matches = p.search(line)
+    character_names[matches.group(1)] = matches.group(2)
+
+me = sys.argv[1:] or ['731', '1862', '153', '562']
+
+print "----- Given that you sing -----"
+print '\n'.join(character_names[id] for id in me)
+
+print "----- You should also sing -----"
+
+suggested_character_ids = fach_me(me)
+
+for character in suggested_character_ids[:20]:
+    id = character['id']
+    name = character_names[id]
+    print "%s: %s (%s)" % (id, name, character['score'])
